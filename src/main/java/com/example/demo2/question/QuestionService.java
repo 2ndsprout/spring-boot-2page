88f6a678accc3c1ad61d548b757c9ab032385c2d
2ndsprout <ralphlauren5243@gmail.com>
2024-04-15 09:39:17 +0900

    list html add and\ thymeleaf apply

--- src/main/java/com/example/demo2/question/QuestionService.java
+++ src/main/java/com/example/demo2/question/QuestionService.java
@@ -3,9 +3,15 @@ package com.example.demo2.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
 
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList () {
+        return this.questionRepository.findAll();
+    }
 }
